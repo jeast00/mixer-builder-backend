1dd2ba947d5e996e9e3f81365324ec89105b4635
JONATHAN EAST <JONATHAN_EAST@YMAIL.COM>
2021-06-03 11:44:11 -0400

    added show method to Mixer Controller and tested data output with rails server

--- app/controllers/mixers_controller.rb
+++ app/controllers/mixers_controller.rb
@@ -3,4 +3,10 @@ class MixersController < ApplicationController
     def index
         render json: Mixer.all
     end
+
+    def show
+        mixer = Mixer.find_by_id(params[:id])
+        render json: mixer
+    end
+
 end
