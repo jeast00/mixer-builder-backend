e168d1d428bcbdb0cf80c97b4341e24ae50bfa42
JONATHAN EAST <JONATHAN_EAST@YMAIL.COM>
2021-06-03 12:01:53 -0400

    added private strong params method to mixer instructions controller

--- app/controllers/mixer_instructions_controller.rb
+++ app/controllers/mixer_instructions_controller.rb
@@ -3,4 +3,12 @@ class MixerInstructionsController < ApplicationController
     def index
         render json: MixerInstruction.all
     end
+
+
+    private
+    
+    def mixer_instruction_params
+        params.require(:mixer_instructions).permit(:instructions, :mixer_id)
+    end
+
 end
