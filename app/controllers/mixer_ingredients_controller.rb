dd7905da727fec063b20744b609d5e3df1e23077
JONATHAN EAST <JONATHAN_EAST@YMAIL.COM>
2021-06-03 11:55:28 -0400

    added private strong params method for mixer ingredients in mixer ingredients controller

--- app/controllers/mixer_ingredients_controller.rb
+++ app/controllers/mixer_ingredients_controller.rb
@@ -3,4 +3,10 @@ class MixerIngredientsController < ApplicationController
     def index
         render json: MixerIngredient.all
     end
+
+    private
+
+    def mixer_ingredient_params
+        params.require(:mixer_ingredients).permit(:ingredient_1, :ingredient_2, :ingredient_3, :ingredient_4, :ingredient_5, :mixer_id)
+    end
 end
