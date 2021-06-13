6c97cc300de4100910a7ce4bcf7cd29a0ffa7879
JONATHAN EAST <JONATHAN_EAST@YMAIL.COM>
2021-06-13 09:27:11 -0400

    updated mixer ingredient serializer

--- app/controllers/mixers_controller.rb
+++ app/controllers/mixers_controller.rb
@@ -10,7 +10,7 @@ class MixersController < ApplicationController
     end
 
     def create
-        
+        #byebug
     end
 
 
--- app/serializers/mixer_ingredient_serializer.rb
+++ app/serializers/mixer_ingredient_serializer.rb
@@ -2,3 +2,5 @@ class MixerIngredientSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :ingredient_1, :ingredient_2, :ingredient_3, :ingredient_4, :ingredient_5, :drink_instructions, :mixer
 end
+
+testing
